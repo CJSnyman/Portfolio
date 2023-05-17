947407f63022d06cd69ae5ede549ce1bf672ace9
CJSnyman <98422173+CJSnyman@users.noreply.github.com>
2023-05-17 04:20:22 +0200

    Update Welcome.js
    Fixed grammar

--- src/components/MainBody Components/Welcome.js
+++ src/components/MainBody Components/Welcome.js
@@ -3,7 +3,7 @@ const Welcome = () => {
         <section className="welcome">
             <h2>WELCOME</h2>
             <p>
-                Hello World! I am Craig Snyman, a Front-End Web Developer based out of Worcester,
+                Hello World! I am Craig Snyman, a Front-End Web Developer based in Worcester,
                 Western Cape. From Table Mountain to mountains of code. I love it all. Being able to
                 create a highly functioning and user-friendly website is what I live for.
             </p>
